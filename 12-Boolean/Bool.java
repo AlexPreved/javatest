1e14d9432d964f7edb25b236553095bf2807403c
Alex <t89137899903@gmail.com>
2017-07-02 00:08:20 +0700

    12/ Boolean example

---
+++ 12-Boolean/Bool.java
@@ -0,0 +1,21 @@
+class Bool {
+	public static void main (String args[]) {
+		boolean b;
+		
+		
+		
+		b= true;
+		System.out.println ("b = " +b);
+		
+		if(b) 
+			System.out.println ("b-true. Use it!");
+		
+		b = false;
+		System.out.println ("b =" + b);
+		if (b)	
+			System.out.println("Xyu");
+		
+		System.out.println ("10>9?  My answer:" + (10>9));
+		
+	}
+}
